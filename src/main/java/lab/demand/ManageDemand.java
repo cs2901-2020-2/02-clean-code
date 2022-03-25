e732410c533fdc99a2f410a3619e36e218c57a2a
Carlos Balbuena <carlos.balbuena@rappi.com>
2022-03-25 17:49:40 -0500

    Add Main Class and Fix POM

--- src/main/java/lab/demand/ManageDemand.java
+++ src/main/java/lab/demand/ManageDemand.java
@@ -1,8 +1,12 @@
+package lab.demand;
+
 import java.util.List;
 
 public class ManageDemand {
+
     public long filledOrders(List<Long> orders, long k){
         //TODO add your code here
         return 0;
     }
+
 }
